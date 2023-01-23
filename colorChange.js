d524a25adbd43a1d8bed6f9839446a7d2eab8f9b
Andrew <76549716+TheAndrew-O@users.noreply.github.com>
2023-01-23 13:32:45 -0600

    Create colorChange.js

---
+++ colorChange.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const switcher = document.querySelector('.myButton');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+});
